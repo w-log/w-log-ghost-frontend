bf169b3b686bba29592c0f3d34716ca1af73819d
Tae-woong Kim <taewoong431@gmail.com>
2022-07-27 21:42:13 +0900

    Create gh-page-deploy.sh

---
+++ gh-page-deploy.sh
@@ -0,0 +1,14 @@
+rm -rf node_modules/.cache
+
+yarn build
+
+touch ./out/.nokekyll
+
+
+now=`date +%D-%T`
+commit_message="gh-page-deploy: $now"
+git add ./out
+git commit -m "ci(gh-pages):$commit_message"
+
+
+git subtree push --prefix out origin gh-pages
