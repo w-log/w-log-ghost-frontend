2eb5b5e423fd5af23f10b8da4208352398a464b8
Tae-woong Kim <taewoong431@gmail.com>
2022-08-01 19:01:17 +0900

    Update gh-page-deploy.sh

--- gh-page-deploy.sh
+++ gh-page-deploy.sh
@@ -4,11 +4,11 @@ yarn build
 
 touch out/.nojekyll
 
-mv ./out ./docs
+mv -f ./out/ ./docs/
 
 now=`date +%D-%T`
 commit_message="gh-page-deploy: $now"
-git add docs
+git add ./docs
 git commit -m "ci(gh-pages):$commit_message"
 
 
