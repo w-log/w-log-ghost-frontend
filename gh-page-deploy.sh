18ffe9313326817f066539bb7a7e1fc76606e98c
Tae-woong Kim <taewoong431@gmail.com>
2022-08-01 18:56:34 +0900

    chore: ignore out

--- gh-page-deploy.sh
+++ gh-page-deploy.sh
@@ -7,8 +7,8 @@ touch out/.nojekyll
 
 now=`date +%D-%T`
 commit_message="gh-page-deploy: $now"
-git add out
+git add docs
 git commit -m "ci(gh-pages):$commit_message"
 
 
-git subtree push --prefix out origin gh-pages
+git push -u origin gh-pages
